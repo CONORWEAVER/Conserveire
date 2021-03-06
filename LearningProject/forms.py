3ff16fd55cc3de03e6b4b26e0ec5e30312334886
Conor Weaver <conorwucc@gmail.com>
2021-03-06 18:02:59 +0000

    *testing file ammendment commit functionality

--- LearningProject/forms.py
+++ LearningProject/forms.py
@@ -16,8 +16,9 @@ class RegistrationForm(UserCreationForm):
             'password1',
             'password2'
         )
+
     def save(self, commit=True):
-        user=super(RegistrationForm, self).save(commit=False)
+        user = super(RegistrationForm, self).save(commit=False)
         user.first_name = self.cleaned_data['first_name']
         user.last_name = self.cleaned_data['last_name']
         user.email = self.cleaned_data['email']
